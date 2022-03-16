ba1eaca80f709db3c5e8394aafeef1e3c9401cf8
Mikael Vito <supervto@Mikaels-MacBook-Air.local>
2022-03-16 20:27:21 +0700

    initianl commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const nama = "Mikael Vito";
+const pekerjaan = "peserta binar academy";
+console.log(`Halo, namaku ${nama}. Aku adalah ${pekerjaan}`);
+
+let nilaiUjianMatematika = 70;
+nilaiUjianMatematika = 100;
+
+console.log(nilaiUjianMatematika);
+
+const apakahSayaPintar = true;
+const apakahSayaBodoh = false;
+
+console.log(apakahSayaPintar);
+console.log(apakahSayaBodoh);
+
+const ktp = {
+    nama: "Mikael Vito",    
+    alamat: {
+        jalan: "JL. Mars No.26",
+        "rt/rw": "06/09"
+    },
+    pekerjaan: [
+        "Peserta Binar Academy",
+        "Pelaajr"
+    ]
+};
+
+console.log(ktp.nama)
+console.log(ktp.alamat)
+console.log(ktp.pekerjaan)
+console.log(ktp["alamat"]["rt/rw"])
+console.log(ktp.alamat["rt/rw"])
+
+
+ktp.pekerjaan.forEach(function(item) {
+    console.log(`${item} adalah pekerjaan ${ktp.nama}`)
+})
